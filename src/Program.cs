a04bbd0c7338344fbd066b703b836debf1525b01
Dmytro Morozov <dmmorozo@microsoft.com>
2022-05-16 16:40:05 +0200

    Update sample code

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
-
-builder.WebHost.ConfigureKestrel((context, options) => options.ListenAnyIP(5001, options => options.UseHttps(context.Configuration.GetValue<string>("TLS_PATH"), context.Configuration.GetValue<string?>("TLS_PASSWORD"))));
+builder.WebHost.UseKestrel((context, options) => options.ListenAnyIP(5001, options => options.UseHttps(context.Configuration.GetValue<string>("TLS_PATH"), context.Configuration.GetValue<string?>("TLS_PASSWORD"))));
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
-
-app.MapGet("/", (IConfiguration configuration) => $"Hello, {configuration.GetValue<string>("NAME", "World")}!");
-
+app.UseHealthChecks("/healthz");
+app.MapGet("/", (HttpRequest request, IConfiguration configuration) => $"Hello {configuration.GetValue<string>("NAME", "World")} from {Environment.MachineName} via {request.Protocol}");
 app.Run();
