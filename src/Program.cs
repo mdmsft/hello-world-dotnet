f292f24b276cee3e84c81c9205351828cc411bb1
Dmytro Morozov <dmmorozo@microsoft.com>
2022-12-21 21:48:39 +0100

    Refactor HTTPS listener

--- src/Program.cs
+++ src/Program.cs
@@ -1,27 +1,9 @@
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.WebHost.UseKestrel((context, options) => options.ListenAnyIP(5001, options =>
-{
-    string? pfxPath = context.Configuration.GetValue<string?>("PFX_PATH");
-    string? pfxPassword = context.Configuration.GetValue<string?>("PFX_PASSWORD");
-    string? pemCrtPath = context.Configuration.GetValue<string?>("PEM_CRT_PATH");
-    string? pemKeyPath = context.Configuration.GetValue<string?>("PEM_KEY_PATH");
-    string? pemPassword = context.Configuration.GetValue<string?>("PEM_PASSWORD");
-
-    if (pfxPath is { Length: > 0 })
-    {
-        options.UseHttps(pfxPath, pfxPassword);
-    }
-
-    else if (pemCrtPath is { Length: > 0 })
-    {
-        X509Certificate2 certificate = pemPassword is { Length: > 0 } ?
-            X509Certificate2.CreateFromEncryptedPemFile(pemCrtPath, pemPassword, pemKeyPath) :
-            X509Certificate2.CreateFromPemFile(pemCrtPath, pemKeyPath);
-        options.UseHttps(certificate);
-    }
-}));
+builder.WebHost.UseKestrel((context, options) => options.ListenAnyIP(5001, options => UseHttps(context, options)));
 builder.Services.AddHealthChecks();
 
 var app = builder.Build();
@@ -36,3 +18,51 @@ app.MapGet("/", (HttpRequest request, IConfiguration configuration, ILogger<Prog
     return $"Hello {name} from {machine} and origin {origin} via {protocol}";
 });
 app.Run();
+
+static void UseHttps(WebHostBuilderContext context, ListenOptions options)
+{
+    string? pfxPath = context.Configuration.GetValue<string?>("PFX_PATH");
+    string? pfxPassword = context.Configuration.GetValue<string?>("PFX_PASSWORD");
+    string? pemCrtPath = context.Configuration.GetValue<string?>("PEM_CRT_PATH");
+    string? pemKeyPath = context.Configuration.GetValue<string?>("PEM_KEY_PATH");
+    string? pemPassword = context.Configuration.GetValue<string?>("PEM_PASSWORD");
+
+    if (pfxPath is { Length: > 0 })
+    {
+        options.UseHttps(pfxPath, pfxPassword);
+    }
+
+    else if (pemCrtPath is { Length: > 0 })
+    {
+        if (pemKeyPath is { Length: > 0 })
+        {
+            var rsa = RSA.Create();
+            using StreamReader reader = File.OpenText(pemKeyPath);
+            rsa.ImportFromPem(reader.ReadToEnd());
+            string subjectPublicKey = Convert.ToBase64String(rsa.ExportSubjectPublicKeyInfo());
+
+            X509Certificate2Collection certificateCollection = new();
+            certificateCollection.ImportFromPemFile(pemCrtPath);
+            foreach (var certificate in certificateCollection)
+            {
+                string certificateSubjectPublicKey = Convert.ToBase64String(certificate.PublicKey.ExportSubjectPublicKeyInfo());
+                if (string.Equals(subjectPublicKey, certificateSubjectPublicKey, StringComparison.Ordinal))
+                {
+                    options.UseHttps(certificate.CopyWithPrivateKey(rsa));
+                    return;
+                }
+            }
+            Console.WriteLine("The provided key does not match the public key for this certificate");
+            Environment.Exit(-1);
+        }
+
+        else
+        {
+            X509Certificate2 certificate = pemPassword is { Length: > 0 } ?
+                X509Certificate2.CreateFromEncryptedPemFile(pemCrtPath, pemPassword) :
+                X509Certificate2.CreateFromPemFile(pemCrtPath);
+        
+            options.UseHttps(certificate);
+        }
+    }
+}
